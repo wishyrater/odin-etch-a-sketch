04f8741b33719af81d4b7d3d1057f150a1565169
Julian Andersen <julian.cesar.andersen@gmail.com>
2024-07-15 03:29:28 +0200

    Add functionality to randomize colour for each interaction

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ function makeGrid(gridSize) {
     const squares = document.querySelectorAll(".square");
 
     squares.forEach(square => square.addEventListener("mouseover", (e) => {
-        e.target.classList.add("hovered");
+        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        const rgb = `rgb(${r},${g},${b})`;
+
+        e.target.style.backgroundColor = rgb;
     }))
 }
 
